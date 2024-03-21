b66975a03449a1788cef3cfdf428d5e3e4f674e2
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-03-21 11:38:49 +0530

    modified image loading

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,19 +9,11 @@ interface CardProps {
 }
 
 const Card: FC<CardProps> = ({ imgUrl }) => {
-  const [loadingContext, setLoadingContext] = useState(true);
-
   return (
-    <div className="bg-black-04 p-6 rounded-lg border border-black-08">
+    <div className="bg-black-04 p-6 rounded-lg border border-black-08 cursor-pointer">
       <div className="relative">
-        <div className={`w-[275px] h-[350px] bg-black animate-pulse rounded-lg ${!loadingContext ? "h-0" : "h-[350px]"}`} />
-        <img
-          src={`https://image.tmdb.org/t/p/original${imgUrl}`}
-          onLoad={() => {
-            setLoadingContext(false);
-          }}
-          className={`w-[275px] object-cover rounded-lg ${loadingContext ? "h-0" : "h-[350px]"}`}
-        />
+        <div className={`absolute top-0 w-[275px] bg-black animate-pulse rounded-lg h-[350px] z-10`} />
+        <img src={`https://image.tmdb.org/t/p/original${imgUrl}`} className={`relative w-[275px] object-cover rounded-lg h-[350px] z-20`} />
       </div>
       <div className="flex justify-between items-center mt-4 text-sm">
         <div className="flex gap-2 py-2 px-4 rounded-full items-center bg-black-01 border border-black-08">
